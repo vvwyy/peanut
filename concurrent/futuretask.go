b3b401e7d417e20b29273a2a0ba2f320488068c4
FlyingDuck <dongshujin.beans@gmail.com>
2019-04-18 20:28:01 +0800

    Update futuretask.go

--- concurrent/futuretask.go
+++ concurrent/futuretask.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	// NEW -> COMPLETING -> NORMAL
-	// NEW -> COMPLETING -> EXCEPTIONAL
+	// NEW -> COMPLETING -> ERROR
 	// NEW -> CANCELLED
 	// NEW -> INTERRUPTING -> INTERRUPTED
 	NIL          = int32(-1)
